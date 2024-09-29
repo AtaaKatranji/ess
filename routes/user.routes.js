7d6989fdf00e580b67a9c7a673a1048002dc51a5
Ataa Katranji <ataa.katranji@gmail.com>
2024-09-29 16:30:01 +0300

    change path

--- routes/user.routes.js
+++ routes/user.routes.js
@@ -3,5 +3,5 @@ const UserController = require('../controllers/user.controller');
 router.post("/register",UserController.register);
 router.post("/login", UserController.login);
 router.get("/show", UserController.show);
-router.get("/test", UserController.test);
+router.get("/api", UserController.test);
 module.exports = router;
